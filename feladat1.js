54c924258ba6421e3f42a0c1719de3ff223385c6
username <zubermarcellpetrik@gmail.com>
2022-09-08 12:14:35 +0200

    commit test

---
+++ feladat1.js
@@ -0,0 +1,9 @@
+console.log("1.feladat");
+let t = [];
+for (let index = 0; index < 10; index++) {
+    t.push(Math.random()*101)+1
+}
+t.sort()
+for (let index = 0; index < t.length; index++) {
+    console.log(t[index])
+}
